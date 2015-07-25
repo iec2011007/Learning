17ac3aed835f8591a16c1778b942934c14b90c7a
Aman <iec2011007@gmail.com>
2015-07-25 13:51:57 +0530

    Theano testing

---
+++ Theano/test1.py
@@ -0,0 +1,10 @@
+__author__ = 'root'
+
+#Program to compute the following using theano
+
+#Computing tanh(x(t).dot(W) + b) elementwise
+
+import theano
+import theano.tensor as T
+import numpy as np
+
